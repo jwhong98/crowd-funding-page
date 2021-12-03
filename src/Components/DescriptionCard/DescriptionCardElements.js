6e271bbdd4db30479c123d464f4e0aed8123dc60
jwhong <jwhong98@gmail.com>
2021-12-03 15:02:23 -0800

    added description

---
+++ src/Components/DescriptionCard/DescriptionCardElements.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const DescriptionContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    background-color: #FAFAFA;
+`;
+
+export const DescriptionWrapper = styled.div`   
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    padding: 50px 30px;
+    background-color: #fff;
+    border-radius: 10px;
+`;
+
+export const Title = styled.h1`
+    margin-bottom: 30px;
+    font-size: 1.5rem;
+`;
+
+export const Content = styled.p`
+    margin-bottom: 30px;
+    color: #4E4E4E;
+    line-height: 1.5;
+`;
--- src/Components/DescriptionCard/index.js
+++ src/Components/DescriptionCard/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { DescriptionContainer, DescriptionWrapper, Title, Content } from './DescriptionCardElements';
 
 const DescriptionCard = () => {
     return (
-        <div>
-            
-        </div>
+        <DescriptionContainer>
+            <DescriptionWrapper>
+                <Title>About this project</Title>
+                <Content>The Mastercraft Bamboo Monitor Riser is a stylish platform that elevates your screen to a more comfortable viewing height.
+                Placing your monitor at eye level has the potential to improve your posture and make you more comfortable white at work,
+                helping you stay focused on the task at hand.</Content>
+                <Content>Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow 
+                notepads, pens, and USB sticks to ve stored under the stand.</Content>
+            </DescriptionWrapper>
+        </DescriptionContainer>
     )
 }
 
